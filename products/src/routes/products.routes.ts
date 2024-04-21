0062c168c4afeb37bbba266a62a8a913a86a4550
Aditi <aditisaggarwins@gmail.comm>
2024-04-21 21:47:27 +0530

    update route

--- products/src/routes/products.routes.ts
+++ products/src/routes/products.routes.ts
@@ -4,5 +4,6 @@ import { productController } from '../controller/products.controller'
 const router = express.Router()
 
 router.post('/create', productController.createNewProduct)
+router.post('/', productController.createNewProduct)
 
 export default router
