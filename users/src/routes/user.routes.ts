6b0011d939b7b4236c0619460247f7612f575de5
Aditi <aditisaggarwins@gmail.comm>
2024-05-08 17:14:31 +0530

    user route

--- users/src/routes/user.routes.ts
+++ users/src/routes/user.routes.ts
@@ -2,37 +2,29 @@ import express, { Response, Request } from 'express';
 import { userController } from '../controllers/user.controller';
 import { authcontroller } from '../controllers/auth.controller';
 import auth from '../utils/auth/user.middleware';
+import validationMiddleware from '../validations/user.validation';
+
 const router = express.Router();
 
-// router.post("/", async (req: Request, res: Response) => {
-//   try {
-//     const newStore = new Store(req.body);
-//     const store = await newStore.save();
-//     res.json(store);
-//   } catch (error) {
-//     res.status(400).json({ error: "failed to create store" });
-//   }
-// });
-
 //Register User
-router.post('/create', userController.createNewUser);
+router.post('/create', (req, res, next) => validationMiddleware(req, res, next, 'user'), userController.createNewUser);
 
 //Login User
-router.post('/login', userController.login);
+router.post('/login', (req, res, next) => validationMiddleware(req, res, next, 'login'), userController.login);
 
 //Find all User
-router.get('/all', auth, userController.findAllUser);
+router.get('/', (req, res, next) => validationMiddleware(req, res, next, 'listing'), auth, userController.findAllUser);
 
 //Find a specific user
-router.get('/:id', userController.findUserById);
+router.get('/:id', (req, res, next) => validationMiddleware(req, res, next, 'user'), userController.findUserById);
 
 //Update a specific user
-router.put('/:id', auth, userController.updateUser);
+router.put('/:id', (req, res, next) => validationMiddleware(req, res, next, 'user'), auth, userController.updateUser);
 
 //Deletes a specific user
 router.delete('/:id', auth, userController.deleteUser);
 
-//ForgetPassword
+//ForgetPasswordS
 router.post('/forgotPassword', authcontroller.forgetPassword);
 // router.route('/forgetPassword').post(authcontroller.forgetPassword);
 
