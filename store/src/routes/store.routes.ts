54d265bc5145f0ac037f315a3e1bae43b19052b8
Aditi <aditisaggarwins@gmail.comm>
2024-05-08 17:16:05 +0530

    store route

--- store/src/routes/store.routes.ts
+++ store/src/routes/store.routes.ts
@@ -1,35 +1,25 @@
 import express, { Response, Request } from 'express';
 //import Store from "../model/store.model";
 import { StoreController } from '../controller/store.controller';
+import validationMiddleware from '../validation/store.validation';
 const router = express.Router();
 
-// router.post("/", async (req: Request, res: Response) => {
-//   try {
-//     const newStore = new Store(req.body);
-//     const store = await newStore.save();
-//     res.json(store);
-//   } catch (error) {
-//     res.status(400).json({ error: "failed to create store" });
-//   }
-// });
-//Route to add new Store
-router.post('/', StoreController.createNewStore);
+//Route to create store
+router.post('/', (req, res, next) => validationMiddleware(req, res, next, 'store'), StoreController.createNewStore);
 
-//route to login
-router.post('/login', StoreController.login);
+//login store
+router.post('/login', (req, res, next) => validationMiddleware(req, res, next, 'login'), StoreController.login);
 
-//route to get all the Store
-router.get('/', StoreController.getAllStore);
+//listing of store
+router.get('/', (req, res, next) => validationMiddleware(req, res, next, 'listing'), StoreController.getAllStore);
 
-//route to get all the store
-router.get('/:id', StoreController.getStoreById);
+///Route to get specific store
+router.get('/:id', (req, res, next) => validationMiddleware(req, res, next, 'store'), StoreController.getStoreById);
 
-//router to update the store
-router.put('/:id', StoreController.updateStore);
+///Route to update store
+router.put('/:id', (req, res, next) => validationMiddleware(req, res, next, 'store'), StoreController.updateStore);
 
-//router to delete Store
+///Route to delete store
 router.delete('/:id', StoreController.deleteStore);
 
-//router to publish the message
-router.post('/publisher', StoreController.publishMessage);
 export default router;
