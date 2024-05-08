5bf76caca669c14358b4d1f7b095d1ed6c27e843
Aditi <aditisaggarwins@gmail.comm>
2024-05-08 17:17:06 +0530

    product route

--- products/src/routes/products.routes.ts
+++ products/src/routes/products.routes.ts
@@ -1,28 +1,29 @@
 import express, { Response, Request } from 'express';
 import { productController } from '../controller/productController';
 import imageStorage from '../utilis/fileUpload/file.upload';
+import validationMiddleware from '../validation/product.validation';
 
 const router = express.Router();
 
 //Route to add new Book
-router.post('/', productController.createNewProduct);
+router.post('/', (req, res, next) => validationMiddleware(req, res, next, 'product'), productController.createNewProduct);
+
+//login product
+router.post('/login', (req, res, next) => validationMiddleware(req, res, next, 'login'));
 
 //route to get all the books
-router.get('/', productController.getAllBooks);
+router.get('/', (req, res, next) => validationMiddleware(req, res, next, 'listing'), productController.getAllBooks);
 
 //Route to get book by Id
-router.get('/:id', productController.getBookById);
+router.get('/:id', (req, res, next) => validationMiddleware(req, res, next, 'listing'), productController.getBookById);
 
 //router to update the book
-router.put('/:id', productController.updateBook);
+router.put('/:id', (req, res, next) => validationMiddleware(req, res, next, 'product'), productController.updateBook);
 
 //router to delete the book
 router.delete('/:id', productController.deleteBookById);
 
 //router to upload image of product
 router.post('/singleImage', imageStorage.single('image'), productController.uploadImage);
-
-//route to subscribe to channel
-router.post('/subscriber', productController.subscribeToStore);
 
 export default router;
